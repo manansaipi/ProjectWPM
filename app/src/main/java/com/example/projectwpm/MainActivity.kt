d6ad3443ac45aea36b651dca3e314e4300efc6f4
manansaipi <boben.manan5@gmail.com>
2022-09-11 11:14:31 +0700

    UPDATE content

--- app/src/main/java/com/example/projectwpm/MainActivity.kt
+++ app/src/main/java/com/example/projectwpm/MainActivity.kt
@@ -2,25 +2,45 @@ package com.example.projectwpm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var nameEditText: EditText
+    private lateinit var submitButton: Button
+    private lateinit var submitTextView: TextView
+
+    private fun initComponent(){
+        nameEditText = findViewById(R.id.nameEditText)
+        submitButton = findViewById((R.id.submitButton))
+        submitTextView = findViewById(R.id.submitTextView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_layout)
 
-        val nameEditText: EditText = findViewById(R.id.nameEditText)
-        val submitButton: Button = findViewById((R.id.submitButton))
-        val submitTextView: TextView = findViewById(R.id.submitTextView)
+        initComponent()
 
-        submitTextView.text = "Hi"
+        submitTextView.text = resources.getString(R.string.app_name)
 
-        submitButton.setOnClickListener {
+         submitButton.setOnClickListener {
+
+             Log.d("PZN", "this is debug log")
+             Log.i("PZN", "this is info log")
+             Log.w("PZN", "this is warning log")
+             Log.e("PZN", "this is error log")
+
             val name = nameEditText.text.toString()
-            submitTextView.text = "Hi $name"
+            submitTextView.text = resources.getString(R.string.submitTextView, name)
+
+             resources.getStringArray(R.array.names).forEach {
+                 Log.i("PZN", it)
+             }
         }
     }
 }
